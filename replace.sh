1c759387e40a1c6d47c1d3f28a898e5b24244de9
chensixiang <chensixiang12345@gmail.com>
2022-06-24 00:45:37 +0800

    部署

--- replace.sh
+++ replace.sh
@@ -2,8 +2,8 @@
 
 find '/usr/share/nginx/html' -name '*.js' -exec sed -i \
   -e "s/BASE_API_HOST/$BASE_API_HOST/g" \
-  -e "/BASE_API_PORT/$BASE_API_PORT/g" \
-  -e "/BASE_API_PROTOCOL/$BASE_API_PROTOCOL/g" \
+  -e "s/BASE_API_PORT/$BASE_API_PORT/g" \
+  -e "s/BASE_API_PROTOCOL/$BASE_API_PROTOCOL/g" \
   {} \;
 
-nginx -g "damon off;"
+nginx -g "daemon off;"
